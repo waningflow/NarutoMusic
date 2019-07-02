5b7b83b6fc6492a4224e4739c41e325eafe2506c
waningflow <jack1995woods@gmail.com>
2019-07-02 13:24:20 +0800

    add modal and fonts

--- app/components/ProfileBar/ProfileBar.js
+++ app/components/ProfileBar/ProfileBar.js
@@ -1,13 +1,53 @@
+// @flow
 import React, { Component } from 'react';
-import Button from 'rsuite/lib/Button';
+import { Button, Modal } from 'rsuite';
 // import styles from './ProfileBar.css';
 
-export default class ProfileBar extends Component {
+type Props = {};
+type State = { showLoginModal: boolean };
+
+export default class ProfileBar extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      showLoginModal: false
+    };
+  }
+
+  close = () => {
+    this.setState({
+      showLoginModal: false
+    });
+  };
+
+  open = () => {
+    this.setState({
+      showLoginModal: true
+    });
+  };
+
   render() {
+    const { showLoginModal } = this.state;
     return (
       <div>
         未登录
-        <Button appearance="primary">login</Button>
+        <Button appearance="primary" onClick={this.open}>
+          login
+        </Button>
+        <Modal show={showLoginModal} onHide={this.close}>
+          <Modal.Header>
+            <Modal.Title>Modal Title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body />
+          <Modal.Footer>
+            <Button onClick={this.close} appearance="primary">
+              Ok
+            </Button>
+            <Button onClick={this.close} appearance="subtle">
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
